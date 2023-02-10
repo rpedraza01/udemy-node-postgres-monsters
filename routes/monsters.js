a83c0671391f77f0331ce5bad15b6b9958e749d7
Rodrigo Pedraza <rpedraza01@gmail.com>
2023-02-10 10:00:37 -0800

    added PUT and pability to update partial data

--- routes/monsters.js
+++ routes/monsters.js
@@ -35,4 +35,30 @@ router.post('/', (request, response, next) => {
     );
 });
 
+router.put('/:id', (request, response, next) => {
+    const { id } = request.params;
+
+    const { name, personality } = request.body;
+
+    const keys = ['name', 'personality'];
+
+    const fields = [];
+
+    keys.forEach(key => {
+        if (request.body[key]) fields.push(key);
+    });
+
+    fields.forEach((field, index) => {
+        pool.query(
+            `UPDATE monsters SET ${field}=($1) WHERE id=($2)`,
+            [request.body[field], id],
+            (err, res) => {
+                if (err) return next(err);
+    
+                if (index === fields.length - 1) response.redirect('/monsters');
+            }
+        )
+    });
+});
+
 module.exports = router;
