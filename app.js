a8137508d0fe3b90b1bda7bdfb1c55639935f938
Rodrigo Pedraza <rpedraza01@gmail.com>
2023-02-10 10:46:18 -0800

    added lives route and refactored all routes into new index.js

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const monsters = require('./routes/monsters');
-const habitats = require('./routes/habitats');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/monsters', monsters);
-app.use('/habitats', habitats);
+app.use('/', routes);
 
 app.use((err, req, res, next) => {
     res.json(err);
---
+++ routes/lives.js
@@ -0,0 +1,26 @@
+// Relational table for joining monsters and habitats
+const { Router, request, response } = require('express');
+const pool = require('../db');
+
+const router = Router();
+
+router.get('/', (request, response, next) => {
+    pool.query('SELECT * FROM lives', (err, res) => {
+        if (err) return next(err);
+
+        response.json(res.rows);
+    });
+});
+
+router.get('/conditions', (request, response, next) => {
+    pool.query(
+        'SELECT * FROM lives JOIN habitats ON habitats.name = lives.habitat',
+        (err, res) => {
+            if (err) return next(err);
+
+            response.json(res.rows);
+        }
+    )
+});
+
+module.exports = router;
